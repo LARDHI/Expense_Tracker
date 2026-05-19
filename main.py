86d2481220cfb52decbffffe9acbbc926f2813e2
AMER LARDHI <amersalmeen@outlook.sa>
2026-05-19 22:31:27 +0300

    feat: add load from file and fixing some bugs

--- main.py
+++ main.py
@@ -10,52 +10,66 @@ def menu():
     choice = input('Enter your choice: ').capitalize()
     return choice
 
-def add_expense(choice, lst: list):
-    expenses = dict()
+def add_expense(expenses_list: list):
+    expenses_data = dict()
     try:
         name = input('Enter the Name of the Expense: ')
         category = input('Enter the Category of the Expense: ')
         amount = input('Enter the Amount of the Expense: ')
-        expenses['Name'] = name.capitalize()
-        expenses['Category'] = category.capitalize()
-        expenses['Amount'] = float(amount)
-        lst.append(expenses)
+        expenses_data['Name'] = name.capitalize()
+        expenses_data['Category'] = category.capitalize()
+        expenses_data['Amount'] = float(amount)
+        expenses_list.append(expenses_data)
     
     except ValueError:
         print('Invalid input. Please enter a valid number for the Amount')
-    return expenses
+    return expenses_data
 
-def view_expenses(choice, expenses: dict, lst: list):
+def view_expenses(expenses_list: list):
     print('Viewing Expenses: ')
-    if len(expenses) < 1:
+    if len(expenses_list) < 1:
         print('No Expenses to Show')
-    for expense in lst:
+    for expense in expenses_list:
         print(f"{expense['Category'].ljust(15)}| {expense['Name'].ljust(15)}| {expense['Amount']}")
 
-def save_file(choice):
+def save_file(choice, expenses_list: list):
     choice = input('Do you want to Save in a New File? (Y/n): ').capitalize()
     if choice == 'Y':
         create_file = input('Enter the name of The File to Save: ')
         with open(create_file, 'w') as file:
-            for expense in lst:
-                file.write(f"{expense['Category'].ljust(15)}| {expense['Name'].ljust(15)}| {expense['Amount']}\n")
+            for expense_data in expenses_list:
+                file.write(f"{expense_data['Category'].ljust(15)}| {expense_data['Name'].ljust(15)}| {expense_data['Amount']}\n")
     elif choice == 'N':
         with open('expenses.txt', 'w') as file:
-            for expense in lst:
-                file.write(f"{expense['Category'].ljust(15)}| {expense['Name'].ljust(15)}| {expense['Amount']}\n")
+            for expense_data in expenses_list:
+                file.write(f"{expense_data['Category'].ljust(15)}| {expense_data['Name'].ljust(15)}| {expense_data['Amount']}\n")
 
-#def load_file(choice):
-lst = list()
+def load_file(expenses_list: list):
+    print('Loading expenses from File: ')
+    expenses_list.clear()
+    try:
+        with open('expenses.txt', 'r') as file_handle:
+            for line in file_handle:
+                parts = line.strip().split('|')
+                if len(parts) == 3:
+                    category = parts[0].strip()
+                    name = parts[1].strip()
+                    amount = float(parts[2].strip())
+                    expenses = {'Category': category, 'Name':name, 'Amount':amount}
+                    expenses_list.append(expenses)
+    except FileNotFoundError:
+        print('No File Found. Please Save Your Expenses to File First')
+expenses_list = list()
 while True:
     choice = menu()
     if choice == '1' or choice == 'Add':
-         expense = add_expense(choice, lst)
+         expenses_data = add_expense(expenses_list)
     elif choice == '2' or choice == 'View':
-        view_expenses(choice, expense, lst)
+        view_expenses(expenses_data, expenses_list)
     elif choice == '3' or choice == 'Save':
         save_file(choice)
-#    elif choice == '4' or choice == 'Load':
-#        load_file(choice)
+    elif choice == '4' or choice == 'Load':
+        load_file(choice)
     elif choice == '5' or choice == 'Exit':
         exit()
     else:
